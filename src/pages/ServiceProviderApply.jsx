69b0226248156b58bf66e9d0540ee4ef66ab805e
nein snitzil <neinsnitzil@gmail.com>
2026-03-12 03:50:21 +0000

    File changes

--- src/pages/ServiceProviderApply.jsx
+++ src/pages/ServiceProviderApply.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { base44 } from '@/api/base44Client';
+import { Link } from 'react-router-dom';
+import { createPageUrl } from '../utils';
 import { motion, AnimatePresence } from 'framer-motion';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
