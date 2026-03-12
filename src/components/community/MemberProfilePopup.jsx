db4f2f141e6793e851b33a75b03eb335f8bfa85d
nein snitzil <neinsnitzil@gmail.com>
2026-03-12 02:52:00 +0000

    File changes

--- src/components/community/MemberProfilePopup.jsx
+++ src/components/community/MemberProfilePopup.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { createPortal } from 'react-dom';
 import { Link } from 'react-router-dom';
 import { createPageUrl } from '../../utils';
 import { base44 } from '@/api/base44Client';
